921dac9dc2d99234771be53159450acfb3fde6c0
Unknown <taru.liukkonen90@gmail.com>
2018-09-10 18:46:14 +0300

    Summary

--- web-api/Startup.cs
+++ web-api/Startup.cs
@@ -26,6 +26,9 @@ namespace web_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IRepository, InMemoryRepository>();
+            services.AddSingleton<PlayersProcessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
